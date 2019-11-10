74e2f2bc3f39a2ecd8ca6a3f10d6edb1be795ae4
Jonathan Trowbridge <jonathan.trowbridge@protonmail.com>
2019-11-10 01:21:53 -0500

    master - Progress...

--- SharpLibTorrent/SharpLibTorrent.cs
+++ SharpLibTorrent/SharpLibTorrent.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using CppSharp;
 using CppSharp.AST;
 using CppSharp.Generators;
@@ -6,9 +8,17 @@ namespace SharpLibTorrent
 {
     public class SharpLibTorrent : ILibrary
     {
+        private static readonly string PathProjectRoot = Path.GetFullPath(Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory
+            , @"../../"
+        ));
+
+        private static readonly string PathLibTorrentRoot = Path.Combine(PathProjectRoot, "Lib/libtorrent/");
+        private static readonly string PathLibTorrentInclude = Path.Combine(PathLibTorrentRoot, "include/libtorrent");
+
+
         public void Preprocess(Driver driver, ASTContext ctx)
         {
-            
         }
 
         public void Postprocess(Driver driver, ASTContext ctx)
@@ -18,8 +28,20 @@ namespace SharpLibTorrent
         public void Setup(Driver driver)
         {
             var options = driver.Options;
+
             options.GeneratorKind = GeneratorKind.CSharp;
+            options.OutputDir = Path.Combine(Environment.CurrentDirectory, "output");
+            options.Verbose = true;
+            options.UseHeaderDirectories = true;
+
             var module = options.AddModule("SharpLibTorrent");
+
+            module.LibraryName = "SharpLibTorrent";
+            module.OutputNamespace = "SharpLibTorrent";
+
+            module.IncludeDirs.Add(PathLibTorrentInclude);
+
+
 //            module.IncludeDirs.Add("C:\Sample\include");
 //            module.Headers.Add("Sample.h");
 //            module.LibraryDirs.Add("C:\Sample\lib");
@@ -29,7 +51,7 @@ namespace SharpLibTorrent
         public void SetupPasses(Driver driver)
         {
         }
-        
+
         public static void GenerateBindings() => ConsoleDriver.Run(new SharpLibTorrent());
     }
 }
